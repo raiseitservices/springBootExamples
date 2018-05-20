110ab118a43318320e0cf7068092017db96e71c6
Parthiban Chinnakkannan <pchinnakkannan@ban-1pchinna-m.local>
2018-05-20 18:35:02 +0530

    Dockerized application and ignored DS_Store file

--- springBootExamples/src/main/java/com/parthi/spring/SpringBootExamplesApp.java
+++ springBootExamples/src/main/java/com/parthi/spring/SpringBootExamplesApp.java
@@ -2,8 +2,13 @@ package com.parthi.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+/*// 
+ * @EnableScheduling ensures Spring Boot background Task executor is created. 
+ */
+@EnableScheduling
 public class SpringBootExamplesApp {
 
 	public static void main(String[] args) {
